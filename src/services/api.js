fc9038dc13f552ede4fa7f40fd71306c05a22f3b
priyaraja32 <rpriyanka31032004@gmail.com>
2026-04-10 09:32:56 +0530

    fix the api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "https://crm-backend-8puj.onrender.com" })
+const API = axios.create({ baseURL: "https://crm-backend-8puj.onrender.com/api" })
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token")
